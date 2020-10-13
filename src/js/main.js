c930cfe9954fa2a3efc7780fb1491c46990dd510
jesse-zonneveld <jessezonneveld@gmail.com>
2020-10-13 14:38:18 -0700

    add modern js workflow components

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+// const moment = require("moment");
+import moment from "moment";
+
+console.log("hello");
+console.log(moment().startOf("day").fromNow());
+
+let bob = "bob";
+console.log(`${bob} is cool`);
